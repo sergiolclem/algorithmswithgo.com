a8649398c1fc8b33f5a2703db75de343fae07c6f
Sérgio Luis <sergiocl@airtm.io>
2022-08-28 20:54:45 -0300

    sum

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,12 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	if numbers == nil {
+		return 0
+	}
+	r := 0
+	for _, i := range numbers {
+		r += i
+	}
+	return r
 }
