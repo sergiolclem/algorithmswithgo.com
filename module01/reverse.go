7d1f870dd3a3f6b0f5abcfba06c6f0f689b4b7cc
Sérgio Luis <sergiocl@airtm.io>
2022-08-28 21:41:10 -0300

    reverse

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,11 +1,19 @@
 package module01
 
+import (
+	"strings"
+)
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
-//   Reverse("cat") => "tac"
-//   Reverse("alphabet") => "tebahpla"
-//
+//	Reverse("cat") => "tac"
+//	Reverse("alphabet") => "tebahpla"
 func Reverse(word string) string {
-	return ""
+	wl := strings.Split(word, "")
+	for i := range wl[0:int(len(wl)/2)] {
+		wl[i], wl[len(wl)-1-i] = wl[len(wl)-1-i], wl[i]
+	}
+
+	return strings.Join(wl, "")
 }
